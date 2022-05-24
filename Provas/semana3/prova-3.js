f988bf8c7e09dd0b2e27010df9635aea273ea459
luedsf <luiz.esf@gmail.com>
2022-05-24 16:39:28 -0300

    adição e atualização de arquivos após tentativa 1 da prova 2/3 semana 3

---
+++ Provas/semana3/prova-3.js
@@ -0,0 +1,35 @@
+// var p = 5; function func() {
+
+//     var p = 9;
+    
+    
+    
+//     function decl() { console.log(p);
+    
+//     }
+    
+//     var expr = function() { console.log(p);
+    
+//     };
+    
+//     var cons = new Function('\tconsole.log(p);');
+    
+    
+    
+//     decl();
+    
+//     expr();
+    
+//     cons();
+    
+//     }
+    
+//     func();
+
+
+// setTimeout(() => console.log("timeout"));
+
+// Promise.resolve().then(() => console.log("promise"));
+
+// console.log("code");
+
